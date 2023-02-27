a8417341d586b5920dd65763b8239ee3bdf38fab
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-02-27 18:49:09 +0530

    changes happen

--- 02.print.sh
+++ 02.print.sh
@@ -1,8 +1,9 @@
 echo Hello World
 
 ## Color Syntax
-# echo -e "\e[COLm MESSAGE" \e[0m"
+# echo -e "\e[COLm MESSAGE \e[0m"
 
-echo -e "\e[31mHello in Red Color"
+echo -e "\e[31mHello in Red Color\e[0m"
 echo Hello World
+
 
