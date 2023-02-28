e205cb7752dd18ef55eef283dc345e4c7c6624d3
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-02-28 15:41:53 +0530

    function changes are

--- 05.function.sh
+++ 05.function.sh
@@ -1,8 +1,12 @@
 ## Declare a function
 xyz() {
   echo Hello from Function
+  echo First Argument - $1
+  echo Second Argument -- $2
+  echo All Arguments - $*
+  echo No of Arguments - $#
 }
 
 #Calling a function
 
-xyz
+xyz 123 456
