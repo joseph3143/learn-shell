fa0ab26e354f2a89ee36d425f9646f82a551eb1f
joseph3143 <jraddala@gmail.com>
2023-06-19 16:52:33 +0530

    Variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,3 +1,7 @@
 # Assign a name to set of data that is called Variable
-DATE=2023-06-19
-echo "Hello Good Morning, ${DATE}"
+#DATE=2023-06-19
+#echo "Hello Good Morning, ${DATE}"
+    # we need to get & take data dynamically
+1. Command Substitution: (mostly used)
+DATE = $(date)
+echo "Hello, Good Morning Date is ${DATE}"
