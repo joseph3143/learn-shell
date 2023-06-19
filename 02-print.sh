e945f35ab8d585919ab22393a9eabcf342d7d259
joseph3143 <jraddala@gmail.com>
2023-06-19 16:31:43 +0530

    Printing

--- 02-print.sh
+++ 02-print.sh
@@ -3,3 +3,6 @@ echo Hello World
 echo -e "\e[35mInstalling Nginx\e[0m"
 
 echo -e "\e[31mHello World in Red Color\e[0m"
+
+echo -e "\e[35m Color Codes from 31-Red , 32-Green, 33-Yellow, 34-Blue, 35-Magenta, 36-Cyan"
+
