65de11f2894ad69ec47d11f1b0eed0d417d91504
joseph3143 <jraddala@gmail.com>
2023-06-22 10:44:49 +0530

    Functions

---
+++ 05-Functions.sh
@@ -0,0 +1,15 @@
+# Variables hold data
+# Functions hold commands
+
+xyz(){                  #Declare a function
+  echo Hello from function
+
+  }
+  xyz                     # Call a function
+
+#print_head(){
+#  echo -e "\e[32m$1\e[0m"
+#}
+
+# Variables => Assign a name to a set of data
+# Functions => Assign a name to set of commands
