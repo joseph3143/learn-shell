f540b9b61de1bf45a32ec94751ad5bec97746897
joseph3143 <jraddala@gmail.com>
2023-06-22 17:08:38 +0530

    Few Changes in Frontend and other

--- 05-Functions.sh
+++ 05-Functions.sh
@@ -3,13 +3,27 @@
 
 xyz(){                  #Declare a function
   echo Hello from function
+  echo First Argument - $1
+  echo Second Argument - $2
+  echo All Arguments - $*
+  echo No of Arguments - $#
+  echo Value of a = $a
+  a-300
+  b=200
   }
-  xyz                     # Call a function
 
-print_head(){
-  echo -e "\e[32m$1\e[0m"
-}
+## Main Program
+# Call a function
+a=120
+xyz 123 456
+echo Value of b - $b
+
+#
+#print_head(){
+#  echo -e "\e[32m$1\e[0m"
+#}
 
 # Variables => Assign a name to a set of data
 # Functions => Assign a name to set of commands
+
 
